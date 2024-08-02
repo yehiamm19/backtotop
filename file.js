919fb6bd95e215933ee8796c724d1731abbe4799
yehiamm19 <155308508+yehiamm19@users.noreply.github.com>
2024-08-02 17:43:16 +0300

    Create file.js

---
+++ file.js
@@ -0,0 +1,35 @@
+    /*---------- 05. Scroll To Top ----------*/
+    // progressAvtivation
+    if($('.scroll-top')) {
+        
+        var scrollTopbtn = document.querySelector('.scroll-top');
+        var progressPath = document.querySelector('.scroll-top path');
+        var pathLength = progressPath.getTotalLength();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+        var updateProgress = function () {
+            var scroll = $(window).scrollTop();
+            var height = $(document).height() - $(window).height();
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+        }
+        updateProgress();
+        $(window).scroll(updateProgress);	
+        var offset = 50;
+        var duration = 750;
+        jQuery(window).on('scroll', function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery(scrollTopbtn).addClass('show');
+            } else {
+                jQuery(scrollTopbtn).removeClass('show');
+            }
+        });				
+        jQuery(scrollTopbtn).on('click', function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        })
+    }
